ae825596f9e3eb3ddb8b66a69a8e49712776fef1
hngngn <huyhoang.hngngn@gmail.com>
2023-11-11 14:48:25 +0700

    fix: clone oh-my-zsh

--- zsh.sh
+++ zsh.sh
@@ -10,7 +10,29 @@ if [ -d $zsh_path ]; then
     rm -rf $zsh_path
 fi
 
-git clone git://github.com/robbyrussell/oh-my-zsh.git $zsh_path
+if [ -n "$zsh_path" ]; then
+    mkdir -p "$zsh_path"
+fi
+
+git init --quiet "$zsh_path" && \
+cd "$zsh_path" && \
+git config core.eol lf && \
+git config core.autocrlf false && \
+git config fsck.zeroPaddedFilemode ignore && \
+git config fetch.fsck.zeroPaddedFilemode ignore && \
+git config receive.fsck.zeroPaddedFilemode ignore && \
+git config oh-my-zsh.remote origin && \
+git config oh-my-zsh.branch "$BRANCH" && \
+git remote add origin "$REMOTE" && \
+git fetch --depth=1 origin && \
+git checkout -b "$BRANCH" "origin/$BRANCH" || {
+    [ ! -d "$zsh_path" ] || {
+        cd -
+        rm -rf "$zsh_path" 2>/dev/null
+    }
+    echo "git clone of oh-my-zsh repo failed"
+    exit 1
+}
 chsh -s $(which zsh)
 
 while read r_plugin; do
